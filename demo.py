8edc6b81f787d376466cc558328997f3ec874bf3
Manan66 <63852645+Manan66@users.noreply.github.com>
2020-04-19 02:42:09 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import time
 while 4 > 2 :
     print("Hello Docker")
-     time.sleep(2)
-     print("OK google , thanks for making your own container !!")
+    time.sleep(2)
+    print("OK google , thanks for making your own container !!")
